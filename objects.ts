0dd055c3e316ddd7b2824f394336a5efd947cffb
samarabdalgadir <uofksam123@gmail.com>
2025-05-30 19:43:10 +0300

    Add files via upload

---
+++ objects.ts
@@ -0,0 +1,13 @@
+objects.ts;
+let book = {
+  title: "Clean Code",
+  author: "Robert C. Martin",
+  yearPublished: 2008,
+};
+console.log("Book:", book);
+let student = {
+  name: "Ahmad",
+  age: 21,
+  isGraduated: false,
+};
+console.log("Student:", student);
